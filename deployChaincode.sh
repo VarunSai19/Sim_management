12da5b549105172ccd0c3d3bbdf838809dc3873f
varunsai99 <varunmessi@gmail.com>
2021-07-29 13:05:14 +0530

    fc

--- deployChaincode.sh
+++ deployChaincode.sh
@@ -230,23 +230,6 @@ chaincodeInvoke() {
 
 # chaincodeInvoke
 
-chaincodeInvokeDeleteAsset() {
-    setGlobalsForPeer0Org1
-
-    # Create Car
-    peer chaincode invoke -o localhost:7050 \
-        --ordererTLSHostnameOverride orderer.example.com \
-        --tls $CORE_PEER_TLS_ENABLED \
-        --cafile $ORDERER_CA \
-        -C $CHANNEL_NAME -n ${CC_NAME}  \
-        --peerAddresses localhost:7051 --tlsRootCertFiles $PEER0_ORG1_CA \
-        --peerAddresses localhost:9051 --tlsRootCertFiles $PEER0_ORG2_CA   \
-        -c '{"function": "DeleteCarById","Args":["2"]}'
-
-}
-
-# chaincodeInvokeDeleteAsset
-
 chaincodeQuery() {
     setGlobalsForPeer0Org1
     peer chaincode query -C $CHANNEL_NAME -n ${CC_NAME} -c '{"function": "ReadCSPData","Args":["test_csp"]}'
